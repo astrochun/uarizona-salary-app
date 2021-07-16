e3b4cc6ce4a4adc3b2996c304479325f3b4d8d25
Chun Ly <astro.chun@gmail.com>
2021-07-15 22:03:16 -0700

    Specify year with first append_to_df call for ii=0
     - Minor PEP8 formatting

--- salary_app/etl.py
+++ salary_app/etl.py
@@ -134,10 +134,9 @@ def set_unique_identifier(list_files: list):
 
         if ii == 0:
             # Initialize with earliest fiscal year data
-            unique_df = append_to_df(df, unique_df, name_list_1)
-            non_unique_df = append_to_df(df, non_unique_df, name_list_2)
-
-            unique_df['year'] = fy
+            unique_df = append_to_df(df, unique_df, name_list_1, year=fy)
+            non_unique_df = append_to_df(df, non_unique_df, name_list_2,
+                                         year=fy)
         else:
             # Get latest unique names match from continuously updated unique_df
             unique_names0 = unique_df['Name']
@@ -146,16 +145,21 @@ def set_unique_identifier(list_files: list):
             # Identify existing unique matches and get list of new matches
             name_list_1_union = set(set(name_list_1) & set(unique_names0))
             name_list_1_new   = set(set(name_list_1) - set(unique_names0))
-            write_file(filename.replace('.csv', '_unique_union.txt'), name_list_1_union)
-            write_file(filename.replace('.csv', '_unique_new.txt'), name_list_1_new)
+            write_file(filename.replace('.csv', '_unique_union.txt'),
+                       name_list_1_union)
+            write_file(filename.replace('.csv', '_unique_new.txt'),
+                       name_list_1_new)
 
             # Check against non-unique
             name_list_1_union2 = set(set(name_list_1) & set(non_unique_names0))
-            write_file(filename.replace('.csv', '_unique_union2.txt'), name_list_1_union2)
+            write_file(filename.replace('.csv', '_unique_union2.txt'),
+                       name_list_1_union2)
 
-            print(f"Number of unique records in unique_df: {len(name_list_1_union)}")
+            print(f"Number of unique records in unique_df: "
+                  f"{len(name_list_1_union)}")
             print(f"Number of new unique records: {len(name_list_1_new)}")
-            print(f"Number of new unique records that is non-unique of unique_df: {len(name_list_1_union2)}")
+            print(f"Number of new unique records that is non-unique of unique_df: "
+                  f"{len(name_list_1_union2)}")
 
             # This is unique names not in unique_df and non_unique_df
             # These are essentially new members to be added to unique_df
