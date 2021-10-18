b6a74ed29547be62ba06ff86ae8febfa9530bd0f
Chun Ly <astro.chun@gmail.com>
2021-10-18 15:24:16 -0700

    Add label for inflation line

--- salary_app/plots.py
+++ salary_app/plots.py
@@ -255,9 +255,16 @@ def percentile_plot(data, bin_size, fy_select: str,
 
     N_bin, percent_bin = np.histogram(data, bins=bins)
 
-    x_inflation = [INFLATION_DATA[fy_select]] * 2
+    fy_inflation = INFLATION_DATA[fy_select]
     y_inflation = [0, max(_percent_norm(N_bin)+5)]
-    s.line(x_inflation, y_inflation, color='red', width=2)
+    s.line([fy_inflation] * 2, y_inflation, color='red', width=2)
+
+    l1 = Label(x=INFLATION_DATA[fy_select], y=y_inflation[1],
+               x_offset=15, y_offset=-5, x_units='data', y_units='data',
+               angle=90/180 * 3.14159, text_font_size='12px',
+               text_align='right', text=f'CPI Inflation, {fy_inflation:.2}%')
+    s.add_layout(l1)
+
     s.y_range.end = y_inflation[1]
 
     s.vbar(x=percent_bin[:-1], top=_percent_norm(N_bin), width=0.95*bin_size,
