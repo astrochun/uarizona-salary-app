2ebb8852205b5ba2a452e2e6fa2692f2e50abb25
Chun Ly <astro.chun@gmail.com>
2021-10-19 16:06:53 -0700

    individual_search_page: Add text on multiples of inflation

--- salary_app/views.py
+++ salary_app/views.py
@@ -305,7 +305,6 @@ def individual_search_page(data_dict: dict, unique_df: pd.DataFrame):
             n_years = float(in_fy_list[-1].split('-')[0].replace('FY', '')) - \
                 float(in_fy_list[0].split('-')[0].replace('FY', ''))
             avg_y2y = 100 * (salary_arr[-1] - salary_arr[0])/salary_arr[0] / n_years
-            st.write(f"_Average year-to-year growth_: {avg_y2y:.2f}%")
 
             # Get average inflation data
             inflation_list = [INFLATION_DATA[key] for key in in_fy_list[1:]]
@@ -313,6 +312,10 @@ def individual_search_page(data_dict: dict, unique_df: pd.DataFrame):
             for infl in inflation_list:
                 cumul_inflation *= 1 + infl / 100
             avg_inflation = (cumul_inflation-1)/n_years * 100
+
+            st.write(f"_Average year-to-year growth_: {avg_y2y:.2f}% "
+                     f"({avg_y2y/avg_inflation:.2f}x inflation)")
+
             st.write(f"_Average year-to-year CPI inflation_: "
                      f"{avg_inflation:.2f}%")
             record_df.insert(len(record_df.columns), 'CPI %',
